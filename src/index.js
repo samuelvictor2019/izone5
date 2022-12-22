39bf334dd9d260b3ee4efb71031cc365e36f92ac
Samuel Victor <50127108+samuelvictor2019@users.noreply.github.com>
2022-12-22 12:58:49 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {Link} from 'react-router-dom'
+import ExpenseEntryItem from './components/ExpenseEntryItem'
+ReactDOM.render(
+ <React.StrictMode>
+ <ExpenseEntryItem />
+ </React.StrictMode>,
+ document.getElementById('root')
+);
